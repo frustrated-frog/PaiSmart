d63c38bd4c0dad41fd04558cc739c35079c746cb
machengqian <394690286@qq.com>
2026-07-25 01:42:07 +0800

    feat: 更新知枢品牌名称与 Logo

--- frontend/src/layouts/modules/global-footer/index.vue
+++ frontend/src/layouts/modules/global-footer/index.vue
@@ -7,7 +7,7 @@ defineOptions({
 <template>
   <DarkModeContainer class="h-full flex-center">
     <a href="https://github.com/itwanger/PaiSmart.git" target="_blank" rel="noopener noreferrer">
-      Copyright MIT © 2025 PaiSmart
+      Copyright MIT © 2025 知枢
     </a>
   </DarkModeContainer>
 </template>
